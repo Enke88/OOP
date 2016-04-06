6cc547e5252206ca1c60a162be2e28343007148d
Enke88 <enke88@mail.ru>
2016-04-06 20:44:21 +0600

    Update library.cpp

--- library.cpp
+++ library.cpp
@@ -1,4 +1,3 @@
-#include "StdAfx.h"
 #include <cstdlib>
 #include <iostream>
 #include "book.h"
@@ -21,65 +20,77 @@ int menu_case()
 
 void main()
 {
-	
-	book **Lib;
+	book **Lib=NULL;
 	int n;
 	int i=0;
-	
 	int menu = menu_case();
 	do
-	switch (menu)
-	
 	{
+		switch (menu)
+
+		{
 		case 1:
-			{
-			cout<<"Enter the quantity of books:";
+		{
+			cout << "Enter the quantity of books:";
 			cin >> n;
-			Lib=new book*[n];
-            cout << "It can be only "; cout<< n; cout<< " books in the library" << endl;
-			break;
-			}
-		case 2:
+			Lib = new book*[n];
+			cout << "It can be only " << n << " books in the library" << endl;
+			for (int i = 0; i < n; i++)
 			{
+				Lib[i] = NULL;
+			}
+			break;
+		}
+		case 2:
+		{
+			for (i = 0; i < n; i++)
+			{
+				if (Lib[i] != NULL)
+					delete Lib[i];
 				book * p = new book();
 				p->Set();
-				Lib[i]=p;
-				delete p;
-				i++;
-				break;
-			}
-		
-       case 3:
-		   {
-            cout << "Enter the number of the book:";
-			cin >> i;
-			cout << "Choosen book is:" << endl;
-			cout << Lib[i];
-            break;
-		   }
-       case 4:
-		   {
-		   int i,j;
-            cout << "List of the books in the library" << endl;
-			for (i=0, j=0; i<n; i++, j++)
-			{
-				cout << j;
-				cout << ". ";
-				cout << Lib[i]; 
-				cout << ";"<< endl;
+				Lib[i] = p;
 			}
 			break;
-		   }
-            
-        case 5:
-			{
-            cout << "Exit the library..." << endl;
-            exit(EXIT_SUCCESS);
-            break;
-			}
+		}
+
+		case 3:
+		{
+			cout << "Enter the number of the book:";
+			cin >> i;
+			cout << "Choosen book is:" << endl;
+			Lib[i]->View();
+			break;
+		}
+		case 4:
+		{
+			int i=0, j;
+			cout << "List of the books in the library" << endl;
+			
+				for (i = 0, j = 1; i < n; i++, j++)
+				{
+					cout << j;
+					cout << ". ";
+					if (Lib[i] != NULL)
+						Lib[i]->View();
+					else
+						cout << "NULL";
+					cout << ";" << endl;
+				}
+			break;
+		}
+
+		case 5:
+		{
+			cout << "Exit the library..." << endl;
+			exit(EXIT_SUCCESS);
+			break;
+		}
 		default:
 			cerr << "You made invalid case." << endl;
-            exit(EXIT_FAILURE);
+			exit(EXIT_FAILURE);
+		}
+		menu = menu_case();
 	}
 	while(i!=5);
 }
