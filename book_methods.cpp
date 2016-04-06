a5cab2bd67bfafbc2d361a94d18435ced901bfe7
Enke88 <enke88@mail.ru>
2016-04-06 20:43:24 +0600

    Update book_methods.cpp

--- book_methods.cpp
+++ book_methods.cpp
@@ -1,27 +1,26 @@
-#include "StdAfx.h"
 #include "book.h"
-	
+
 book::book()
 {
-	strcpy(name,"");
-	strcpy(author,"");
-	f=0;
+	strcpy(name, "");
+	strcpy(author, "");
+	f = 0;
 }
-
 void book::View()
 {
-	cout << name << endl;
+	cout << name << ", ";
 	cout << author << endl;
-	if (f==1)
+	if (f == 1)
 		cout << "Book is given" << endl;
 	else
-		cout<< "Book is avaliable" << endl;
+		cout << "Book is avaliable" << endl;
 }
 void book::Set()
 {
 	cout << "Enter the title, please:";
-	cin.getline(name, sizeof(name));
+	cin >> name;
+	cout << "\n";
 	cout << "Enter the author, please:";
-	cin.getline(author, sizeof(author));
+	cin >>author;
 	f = rand() % 2;
 }
